6b91021bdbac736db50ab238031cfcad2630275f
cjd <connor_duffy@hotmail.com>
2021-08-13 17:28:14 -0700

    - adding comments to help guide where to put changes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const router = express.Router();
+// add additional requirements here e.g. requirements, uml, user stories
 const projectsRoute = require('./projects');
 
 module.exports = (params) => {
@@ -8,6 +9,7 @@ module.exports = (params) => {
     response.render('pages/index', { pageTitle: 'Welcome' });
   });
 
+  // add additional routes here e.g. requirements, uml, user stories
   router.use('/projects', projectsRoute(params));
 
   return router;
