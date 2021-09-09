658a71f499898d504e43ccb089b1b105dd25bc1a
cjd <connor_duffy@hotmail.com>
2021-09-08 18:22:14 -0700

    - setting up create requirements page

--- routes/requirements.js
+++ routes/requirements.js
@@ -5,18 +5,26 @@ const router = express.Router();
 module.exports = (params) => {
   const { requirementsService } = params;
 
-  router.get('/', async (request, response) => {
-    return response.render('pages/requirements', {
-      pageTitle: 'Requirements List',
-    });
-  });
-
+  // CREATE
   router.post('/', async (request, response) => {
     console.log(request.body);
     const { name, description } = request.body;
     await projectService.addProject(name, description);
 
     return response.redirect('projects');
+  });
+
+  // READ
+  router.get('/', async (request, response) => {
+    return response.render('pages/requirements', {
+      pageTitle: 'Requirements List',
+    });
+  });
+
+  router.get('/', async (request, response) => {
+    return response.render('pages/requirements', {
+      pageTitle: 'Create Requirement',
+    });
   });
 
   return router;
